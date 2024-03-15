83d574e48ac492e894d6b0a721c4e82cc71f770e
Deepshikha <deepshikha.champawat@gmail.com>
2024-03-15 22:07:14 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,181 @@
+import React from 'react'
+// import VaccinationDetails from './VaccinationDetails'
+import './App.css'
+
+const App = () => {
+  return (
+      <div className="desktop-container">
+        <div className="desktop-desktop">
+          <div className="desktop-desktop1">
+            <img
+              alt="Rectangle328"
+              src="/external/rectangle328-8kx-1100h.png"
+              className="desktop-rectangle3"
+            />
+            <img
+              alt="unsplashSfDofjXtxHE29"
+              src="/external/unsplashsfdofjxtxhe29-hms-1100h.png"
+              className="desktop-unsplash-sf-dofj-xtx-he"
+            />
+            <div className="desktop-group4">
+              <span className="desktop-text">
+                <span>
+                  <span>Vaccination.ng will help you find the nearest</span>
+                  <br></br>
+                  <span>
+                    centre for vaccination, in all 36 states in Nigeria.
+                  </span>
+                </span>
+              </span>
+              <span className="desktop-text05">
+                <span className="desktop-text06">
+                  <span>COVID-19 Vaccination</span>
+                  <br></br>
+                  <span>Got Easier With,</span>
+                  <br></br>
+                  <span></span>
+                </span>
+                <span className="text-color">Vaccination.ng</span>
+              </span>
+              {/* <span className="desktop-text13">
+                <span className="desktop-text14">
+                  <span>COVID-19 Vaccination</span>
+                  <br></br>
+                  <span>Got Easier With,</span>
+                  <br></br>
+                  <span className='desktop-text-13-2'>Vaccination.ng</span>
+                </span>
+              </span> */}
+            </div>
+            <div className="desktop-group7">
+              <div className="desktop-get-vaccine">
+                <span className="desktop-text21">
+                  <span>Get Vaccine</span>
+                </span>
+              </div>
+              <div className="desktop-get-vaccine1">
+                <span className="desktop-text23">
+                  <span>Help Centre</span>
+                </span>
+              </div>
+            </div>
+            <div className="desktop-group16">
+              <div className="desktop-group15">
+                <img
+                  alt="Rectangle4235"
+                  src="/external/rectangle4235-xs7r-200h.png"
+                  className="desktop-rectangle4"
+                />
+                <div className="desktop-group14">
+                  <div className="desktop-group11">
+                    <div className="desktop-group10">
+                      <span className="desktop-text25">
+                        <span>Location</span>
+                      </span>
+                      <span className="desktop-text27">
+                        <span>Ikeja Lagos, Nigeria</span>
+                      </span>
+                    </div>
+                    <div className="desktop-group6">
+                      <img
+                        alt="mappin250"
+                        src="/external/mappin250-e4vd.svg"
+                        className="desktop-mappin"
+                      />
+                    </div>
+                  </div>
+                  <div className="desktop-group12">
+                    <div className="desktop-group101">
+                      <span className="desktop-text29">
+                        <span>Date</span>
+                      </span>
+                      <span className="desktop-text31">
+                        <span>29 February, 2022</span>
+                      </span>
+                    </div>
+                    <div className="desktop-group71">
+                      <img
+                        alt="calendar258"
+                        src="/external/calendar258-e2vr.svg"
+                        className="desktop-calendar"
+                      />
+                    </div>
+                  </div>
+                  <div className="desktop-group13">
+                    <div className="desktop-group102">
+                      <span className="desktop-text33">
+                        <span>Vaccine Type</span>
+                      </span>
+                      <span className="desktop-text35">
+                        <span>Mordena</span>
+                      </span>
+                    </div>
+                    <div className="desktop-group9">
+                      <img
+                        alt="shield266"
+                        src="/external/shield266-nf1.svg"
+                        className="desktop-shield"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="desktop-get-vaccine2">
+                  <span className="desktop-text37">
+                    <span>Submit</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="desktop-group22">
+              <img
+                alt="unsplashwnSEwHMhxho298"
+                src="/external/unsplashwnsewhmhxho298-4hk4-600w.png"
+                className="desktop-unsplashwn-s-ew-h-mhxho"
+              />
+            </div>
+          </div>
+          <div className="desktop-group82">
+            <div className="desktop-results">
+              <img
+                alt="unsplashSfDofjXtxHE59"
+                src="/external/unsplashsfdofjxtxhe59-67n-1500w.png"
+                className="desktop-unsplash-sf-dofj-xtx-he1"
+              />
+              <div className="desktop-group30">
+                <img
+                  alt="Rectangle4510"
+                  src="/external/rectangle4510-h68e-300h.png"
+                  className="desktop-rectangle41"
+                />
+                <div className="desktop-get-vaccine3">
+                  <span className="desktop-text39">
+                    <span>Check</span>
+                  </span>
+                </div>
+                <div className="desktop-group29">
+                  <span className="desktop-text41">
+                    <span>Check your COVID-19 result on our Database</span>
+                  </span>
+                </div>
+                <div className="desktop-group27">
+                  <div className="desktop-group25">
+                    <span className="desktop-text43">
+                      <span className='desktop-text43-box'>Okeowo</span>
+                    </span>
+                  </div>
+                  <div className="desktop-group26">
+                    <span className="desktop-text45">
+                      <span className='desktop-text45-box'>NIK Number</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  
+}
+
+export default App;
---
+++ src/VaccinationDetails.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// function VaccinationDetails() {
+//     const customStyle = {
+//         color: '#17C2EC',
+//         background:'fffff'
+//     };
+
+//     return (
+//         <span className="desktop-text05" style={customStyle}>Vaccination.ng</span>
+//     );
+// }
+
+// export default VaccinationDetails;
